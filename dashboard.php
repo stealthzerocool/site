775b8548aec6b89d83f242aba70ed280823a677a
Ananthu K <ananthuk11@gmail.com>
2021-11-02 05:12:16 -0700

    c

--- dashboard.php
+++ dashboard.php
@@ -13,23 +13,11 @@ if (isset($_SESSION['id']) && isset($_SESSION['user_name'])) {
 <a class="Menu" href="home">Home</a>
 <a href="#news">News</a>
 
-</div>
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-
-<!-- Top Navigation Menu -->
-<div class="topnav">
-  <a href="#home" class="active">Logo</a>
-  <!-- Navigation links (hidden by default) -->
   <div id="myLinks">
-    <a href="#account">Account</a>
+    <a href="#account">Accounts</a>
 
   </div>
- 
-  <a href="javascript:void(0);" class="icon" onclick="myFunction()">
-    <i class="fa fa-bars"></i>
-  </a>
 </div>
-
 
      <h1>Hello, <?php echo $_SESSION['user_name']; ?></h1>
      <a href="logout.php">Logout</a>
