da2db598fdaf15bdd6dea3abd5e24cf55642d5a3
Ananthu K <ananthuk11@gmail.com>
2021-11-03 07:29:51 +0530

    dashb

--- dashboard.php
+++ dashboard.php
@@ -10,17 +10,54 @@ if (isset($_SESSION['id']) && isset($_SESSION['user_name'])) {
 </head>
 <body>
 <div class="topnav">
+<a href="logout.php">Logout</a>
+<a href="profile.php">Profile</a>
+
 <a class="Menu" href="home">Home</a>
-<a href="#news">News</a>
+<!-- <a href="explore.php">Explore</a>
+!-->
 
-  <div id="myLinks">
-    <a href="#account">Accounts</a>
 
-  </div>
+
+</div class="hi1" >
+
+  <h2>Hello, <?php echo $_SESSION['user_name']; ?></h2>
 </div>
+<!--
+<div class=row>
+<div class="column" id="col_left">
+    <h2>Column 1</h2>
+    <p>Some text..</p>
+  </div>
+  <div class="column" id="col_right">
+    <h2>Column 2</h2>
+    <p>Some text..</p>
+  </div>
 
-     <h1>Hello, <?php echo $_SESSION['user_name']; ?></h1>
-     <a href="logout.php">Logout</a>
+</div>
+!-->
+<table width="90%">
+	<col style="width:10%">
+	<col style="width:70%">
+	<thead>
+	<tr>
+		<th><h3>Friends</h3></th>
+		<th>Feed</th>
+	</tr>
+	</thead>
+  
+	<tbody>
+  <tr></tr>
+	<tr>
+		<th><p>sdfasdf asdfa sdf asdf</p></th>
+		<td>98 ui</td>
+	</tr>
+	<tr>
+		<th>Oranges</th>
+		<td>295 ui</td>
+	</tr>
+	</tbody>
+</table>
 </body>
 </html>
 <?php 
--- index.php
+++ index.php
@@ -2,29 +2,45 @@
 <html>
 <head>
     <title>Login</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-    <h2>Login</h2>
+
     <!--<a href ="login.php"> Login</a> 
     !-->
-    <form action="login.php" method ="post">
-    <?php if(isset($_GET['error'])){?>
-    <p class="error"> <?php echo $_GET['error']; ?></p>
-    <?php } ?>
-        <label>Username</label><br>
-        <input type="text" name="uname" placeholder="User Name"><br><br>
+    <div class="row">
+    <div class="column" >
+        <div class="head_a">
+            <h1>Welcome</h1>
+        </div>
+    </div>
+    <div class="column">
+        <div class="col2">
+        <div class="login_pg">
+        <h2>Login</h2>
+        <form action="login.php" method ="post">
+        <?php if(isset($_GET['error'])){?>
+        <p class="error"> <?php echo $_GET['error']; ?></p>
+        <?php } ?>
+            <!-- <label>Username</label><br>
+            !-->
+            <input type="text" name="uname" placeholder="User Name"><br><br>
 
+            <!--
+            <label>Password</label><br>
+            !-->
+            <input type="password" name="password" placeholder="Password"><br><br>
+            
+            <button class="button1" type="submit">Login</button>
+            
+            <!---
+            <input type="text">
+            <input type="submit">
+            --->
         
-        <label>Password</label><br>
-        <input type="password" name="password" placeholder="Password"><br><br>
-
-        <button type="submit">Login</button>
-        
-        <!---
-        <input type="text">
-        <input type="submit">
-        --->
-    
-    </form>    
+        </form>   
+    </div>
+        </div>
+</div> 
 </body>
 </html>
